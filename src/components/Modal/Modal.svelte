c843629f52a00bb85d5bc514b0f4db77a8304065
Raffaele Morí <mori.raffaele@gmail.com>
2021-12-10 00:37:48 +0000

    Added modal for win message

---
+++ src/components/Modal/Modal.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher();
+  function tapOnBackground(evt) {
+    if (this !== evt.target) {
+      return;
+    }
+    dispatch('tapOnBackground');
+  }
+</script>
+
+<div class="modal-main" on:click={tapOnBackground}>
+  <div class="modal-content">
+    <slot />
+  </div>
+</div>
+
+<style>
+  .modal-main {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: rgba(100, 100, 100, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .modal-content {
+    background-color: white;
+    padding: 30px;
+  }
+</style>
--- src/screens/Game/Game.svelte
+++ src/screens/Game/Game.svelte
@@ -1,250 +1,267 @@
 <script>
-    import Icon from 'svelte-awesome';
-    import { home } from 'svelte-awesome/icons';
-    import { getRandomIcons } from '../../lib/icons';
-    import { shuffle } from '../../lib/array';
-    const gameIcons = getRandomIcons();
+  import Modal from '../../components/Modal/Modal.svelte';
+  import Icon from 'svelte-awesome';
+  import { home } from 'svelte-awesome/icons';
+  import { getRandomIcons } from '../../lib/icons';
+  import { shuffle } from '../../lib/array';
+  const gameIcons = getRandomIcons();
 
-    import {bestTimesMemory, saveBestTimeMemory} from '../../stores/bestTimes';
-    let recordTime;
-    bestTimesMemory.subscribe(value => recordTime = value);
+  import { bestTimesMemory, saveBestTimeMemory } from '../../stores/bestTimes';
+  let recordTime;
+  bestTimesMemory.subscribe((value) => (recordTime = value));
 
-    import {pop} from 'svelte-spa-router'
-    import { onMount } from 'svelte';
-    export let params;
-    let selectedCard = -1;
-    let isTemporaryBlocked = false;
-    let numOfCouples;
-    let cards;
-    let cardMap;
-    let totalPaired;
-    let timeStart;
-    let timeDiff = 0;
-    let intervalId;
-    let won = false;
+  import { pop } from 'svelte-spa-router';
+  import { onMount } from 'svelte';
+  export let params;
+  let selectedCard = -1;
+  let isTemporaryBlocked = false;
+  let numOfCouples;
+  let cards;
+  let cardMap;
+  let totalPaired;
+  let timeStart;
+  let timeDiff = 0;
+  let intervalId;
+  let won = false;
 
-    onMount(()=>{
-        // console.log('on mount');
-        // startGame(params);
-    });
+  // onMount(() => {
+  //   // console.log('on mount');
+  //   // startGame(params);
+  // });
 
-    $: numOfCards = params.cards;
-    $: unPair = params.unPair;
+  // $: numOfCards = params.cards;
+  // $: unPair = params.unPair;
 
-    $: {
-        startGame(params);
+  $: {
+    startGame(params);
+  }
+
+  $: {
+    won = totalPaired === numOfCouples;
+    if (won) {
+      saveBestTimeMemory(params.cards, params.unPair, +timeDiff);
+      clearInterval(intervalId);
+      intervalId = null;
     }
+  }
+  function startGame(params) {
+    numOfCouples = (params.cards - params.unPair) / 2;
+    resetGame();
+  }
 
-    $: {
-        won = totalPaired === numOfCouples;
-        if (won) {
-            saveBestTimeMemory(params.cards, params.unPair, +timeDiff);
-            clearInterval(intervalId);
-            intervalId = null;
-        }
-    }
-    function startGame(params) {
-        numOfCouples = (params.cards - params.unPair)/2;
-        resetGame();
-    }
+  function resetGame() {
+    totalPaired = 0;
+    cardMap = getCardMap(params.cards);
+    cards = createCards(numOfCouples, params.unPair);
+    timeStart = null;
+    intervalId = null;
+    timeDiff = '0.00';
+  }
+  $: cardLines = getCardLines(cards, cardMap);
 
-    function resetGame() {
-        totalPaired = 0;
-        cardMap = getCardMap(params.cards);
-        cards = createCards(numOfCouples, params.unPair);
-        timeStart = null;
-        intervalId = null;
-        timeDiff = '0.00';
-    }
-    $: cardLines = getCardLines(cards, cardMap);
+  function getCardMap(mapSize) {
+    const sortedArray = Array.from({ length: mapSize }).map((_, i) => i);
 
-    function getCardMap(mapSize) {
-        const sortedArray = Array.from({length: mapSize})
-            .map((_, i) => i);
+    return shuffle(sortedArray);
+  }
 
-        return shuffle(sortedArray);
-    }
+  function createCards(numOfCouples, numOfUnpair) {
+    const completeCouples = Array.from({ length: numOfCouples })
+      .map((_, i) => [i, i])
+      .flat();
+    const notRepeated = Array.from({ length: numOfUnpair }).map(
+      (_, i) => i + numOfCouples
+    );
+    return [...completeCouples, ...notRepeated].map((value, index) => ({
+      index,
+      value,
+      status: 'hidden',
+    }));
+  }
 
-    function createCards(numOfCouples, numOfUnpair) {
-        const completeCouples = Array.from({length: numOfCouples}).map((_, i) => [i, i]).flat();
-        const notRepeated = Array.from({length: numOfUnpair}).map((_, i) => i + numOfCouples);
-        return [...completeCouples, ...notRepeated].map((value, index) => ({
-            index,
-            value,
-            status: 'hidden'
-        }));
-    }
+  function getCardLines(cards, cardMap) {
+    const lineSizes = getLineSizes(cards);
+    const shafledCards = cardMap.map((i) => cards[i]);
+    return lineSizes.map((lineSize) => shafledCards.splice(0, lineSize));
+  }
 
-    function getCardLines(cards, cardMap) {
-        const lineSizes = getLineSizes(cards);
-        const shafledCards = cardMap.map(i => cards[i]);
-        return  lineSizes.map(lineSize => 
-            shafledCards.splice(0, lineSize)
-        );
-    }
+  function getLineSizes(totalCards) {
+    const numOfCards = totalCards.length;
+    const minNumOfLines = Math.trunc(Math.sqrt(numOfCards));
+    const isMinNumOfLinesValid =
+      numOfCards <= minNumOfLines * (minNumOfLines + 1);
+    const numOfLines = isMinNumOfLinesValid ? minNumOfLines : minNumOfLines + 1;
+    const extraCards = numOfCards - numOfLines * minNumOfLines;
+    const lineSizes = Array.from({ length: numOfLines }).map(
+      (_, i) => minNumOfLines + (i < extraCards)
+    );
+    return lineSizes;
+  }
 
-    function getLineSizes(totalCards) {
-        const numOfCards = totalCards.length;
-        const minNumOfLines = Math.trunc(Math.sqrt(numOfCards));
-        const isMinNumOfLinesValid = numOfCards <= minNumOfLines*(minNumOfLines+1);
-        const numOfLines = isMinNumOfLinesValid ? minNumOfLines : minNumOfLines + 1;
-        const extraCards = numOfCards - numOfLines * minNumOfLines;
-        const lineSizes = Array.from({length: numOfLines}).map((_, i) => minNumOfLines + (i<extraCards));
-        return lineSizes;
-    }
+  function updateCard(index, status) {
+    cards = cards.map((c, i) => (i === index ? { ...c, status } : c));
+  }
 
-    function updateCard(index, status) {
-        cards = cards.map((c, i) => i === index ? {...c, status} : c);
-    }
+  function startTime() {
+    timeStart = new Date();
+    intervalId = setInterval(() => {
+      const diff = (new Date() - timeStart) / 1000;
+      timeDiff = diff.toFixed(2);
+    }, 10);
+  }
 
-    function startTime() {
-        timeStart = new Date();
-        intervalId = setInterval(() => {
-            const diff = (new Date() - timeStart)/1000;
-            timeDiff = diff.toFixed(2);
-        }, 10);
+  function handleCardClick(card) {
+    if (!timeStart) {
+      startTime();
     }
-
-    function handleCardClick(card) {
-        if (!timeStart) {
-            startTime();
-        }
-        const cardStatus = card.status;
-        if (cardStatus !== 'hidden' || isTemporaryBlocked) {
-            return;
-        }
-        updateCard(card.index, 'tmp-shown');
-        if (selectedCard === -1) {
-            selectedCard = card.index;
-            return;
-        }
-        if (card.value === cards[selectedCard].value) {
-            updateCard(card.index, 'paired');
-            updateCard(selectedCard, 'paired');
-            selectedCard = -1;
-            totalPaired = cards.filter(c => c.status === 'paired').length / 2;
-            return;
-        }
-        updateCard(selectedCard, 'wrong');
-        updateCard(card.index, 'wrong');
-        isTemporaryBlocked = true;
-        setTimeout(() => {
-            updateCard(selectedCard, 'hidden');
-            updateCard(card.index, 'hidden');
-            isTemporaryBlocked = false;
-            selectedCard = -1;
-        }, 800);
+    const cardStatus = card.status;
+    if (cardStatus !== 'hidden' || isTemporaryBlocked) {
+      return;
     }
+    updateCard(card.index, 'tmp-shown');
+    if (selectedCard === -1) {
+      selectedCard = card.index;
+      return;
+    }
+    if (card.value === cards[selectedCard].value) {
+      updateCard(card.index, 'paired');
+      updateCard(selectedCard, 'paired');
+      selectedCard = -1;
+      totalPaired = cards.filter((c) => c.status === 'paired').length / 2;
+      return;
+    }
+    updateCard(selectedCard, 'wrong');
+    updateCard(card.index, 'wrong');
+    isTemporaryBlocked = true;
+    setTimeout(() => {
+      updateCard(selectedCard, 'hidden');
+      updateCard(card.index, 'hidden');
+      isTemporaryBlocked = false;
+      selectedCard = -1;
+    }, 800);
+  }
 </script>
 
 <div class="page">
-    <div class="first-line">
-        <div class="home-button" on:click={pop}>
-            <Icon data={home} />
-        </div>
-        <div class="timer">
-            {timeDiff || '0.00'} secs
-        </div>
-        <div class="partial-result">
-            {totalPaired}/{numOfCouples}
-        </div>
+  <div class="first-line">
+    <div class="home-button" on:click={pop}>
+      <Icon data={home} />
     </div>
-    <div class="game-grid">
-        {#each cardLines as cardLine}
-            <div class="game-line">
-                {#each cardLine as singleCard}
-                    <div class="card status--{singleCard.status}" on:click={() => handleCardClick(singleCard)}>
-                        {#if singleCard.status !== 'hidden'}
-                            <Icon data={gameIcons[singleCard.value]} scale="2" />
-                        {/if}
-                        
-                    </div>
-                {/each}
-            </div>
+    <div class="timer">
+      {timeDiff || '0.00'} secs
+    </div>
+    <div class="partial-result">
+      {totalPaired}/{numOfCouples}
+    </div>
+  </div>
+  <div class="game-grid">
+    {#each cardLines as cardLine}
+      <div class="game-line">
+        {#each cardLine as singleCard}
+          <div
+            class="card status--{singleCard.status}"
+            on:click={() => handleCardClick(singleCard)}
+          >
+            {#if singleCard.status !== 'hidden'}
+              <Icon data={gameIcons[singleCard.value]} scale="2" />
+            {/if}
+          </div>
         {/each}
-    </div>
-    {#if won}
-    <div class="win-disclaimer">
-        <div>You won</div>
-        <button on:click={resetGame}>Reset</button>
-    </div>
-    {/if}
+      </div>
+    {/each}
+  </div>
+  {#if won}
+    <Modal on:tapOnBackground={resetGame}>
+      <h3 class="modal-title">CONGRATULATIONS:</h3>
+      <h4 class="modal-sub-title">You won</h4>
+      <div>
+        <button on:click={resetGame}>RESET GAME</button>
+        <button on:click={pop}>GO TO HOME PAGE</button>
+      </div>
+    </Modal>
+  {/if}
 </div>
 
-
 <style>
-    .page {
-        width: 100%;
-        height: 90vh;
-        display: flex;
-        flex-direction: column;
-        background-color: azure;
-    }
+  .page {
+    width: 100%;
+    height: 90vh;
+    display: flex;
+    flex-direction: column;
+    background-color: azure;
+  }
 
-    .game-line {
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        justify-content: center;
-    }
+  .game-line {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
 
-    .game-grid {
-        padding: 2vh 2vw;
-    }
+  .game-grid {
+    padding: 2vh 2vw;
+  }
 
-    .game-field {
-        display: flex;
-        flex-direction: row;
-        flex-wrap: wrap;
-        justify-content: center;
-    }
+  .game-field {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
 
-    .card.status--wrong {
-        border-color: red;
-    }
+  .card.status--wrong {
+    border-color: red;
+  }
 
-    .card {
-        width: 10vw;
-        height: 10vh;
-        text-align: center;
-        margin: 1%;
-        background-color: gray;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border: 5px solid transparent;
-        font-weight: bolder;
-        border-color: orange;
-        border-radius: 5px;
-    }
+  .card {
+    width: 10vw;
+    height: 10vh;
+    text-align: center;
+    margin: 1%;
+    background-color: gray;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 5px solid transparent;
+    font-weight: bolder;
+    border-color: orange;
+    border-radius: 5px;
+  }
 
-    .card.status--hidden {
-        border-color: transparent;
-    }
+  .card.status--hidden {
+    border-color: transparent;
+  }
 
-    .card.status--paired {
-        border-color: green;
-        color: blue;
-        background-color: cyan;
-    }
-    .first-line {
-        display: flex;
-        flex-direction:row;
-        justify-content: space-between;
-        background-color: lightseagreen;
-        color: white;
-        align-items: center;
-        padding: 1vh 2vw;
-    }
-    .partial-result,
-    .home-button,
-    .timer {
-        background-color: rgba(255, 0, 0, .5);
-        padding: 6px;
-        border-radius: 6px;
-    }
+  .card.status--paired {
+    border-color: green;
+    color: blue;
+    background-color: cyan;
+  }
+  .first-line {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    background-color: lightseagreen;
+    color: white;
+    align-items: center;
+    padding: 1vh 2vw;
+  }
+  .partial-result,
+  .home-button,
+  .timer {
+    background-color: rgba(255, 0, 0, 0.5);
+    padding: 6px;
+    border-radius: 6px;
+  }
 
-    .win-disclaimer {
-        background-color: rgba(255, 0, 0, .5);
-    }
+  .win-disclaimer {
+    background-color: rgba(255, 0, 0, 0.5);
+  }
+
+  .modal-title {
+    text-align: center;
+  }
+  .modal-sub-title {
+    text-align: center;
+  }
 </style>
